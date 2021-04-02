acaffb478839f7af5f4c83ae4f869d1403484c8e
dannyb2018 <39492136+dannyb2018@users.noreply.github.com>
2021-04-02 16:38:42 +0900

    now uses jakarta imports

--- nifi-CPTAYahooDataProviderProcessor-processors/src/main/java/com/cloudpta/quantpipeline/backend/data_provider/yahoo/processors/CPTAYahooDataProviderProcessor/request_response/CPTAYahooMessage.java
+++ nifi-CPTAYahooDataProviderProcessor-processors/src/main/java/com/cloudpta/quantpipeline/backend/data_provider/yahoo/processors/CPTAYahooDataProviderProcessor/request_response/CPTAYahooMessage.java
@@ -23,13 +23,13 @@ import com.cloudpta.quantpipeline.api.instrument.symbology.CPTAInstrumentSymbolo
 import com.cloudpta.quantpipeline.backend.data_provider.request_response.CPTADataMessage;
 import com.cloudpta.quantpipeline.backend.data_provider.request_response.CPTADataProperty;
 import com.cloudpta.utilites.exceptions.CPTAException;
+import jakarta.json.Json;
+import jakarta.json.JsonArrayBuilder;
+import jakarta.json.JsonObject;
+import jakarta.json.JsonReader;
 import java.io.StringReader;
 import java.util.HashSet;
 import java.util.List;
-import javax.json.Json;
-import javax.json.JsonArrayBuilder;
-import javax.json.JsonObject;
-import javax.json.JsonReader;
 import jakarta.ws.rs.client.Client;
 import jakarta.ws.rs.client.Invocation;
 import jakarta.ws.rs.client.WebTarget;
